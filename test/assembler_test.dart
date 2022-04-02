ae874d1d17354a1b77b1b4df5337efeca1d41138
npsulav <conexdigicamp@gmail.com>
2022-04-02 21:06:28 +0545

    Assembler opcode hashmap created

--- test/assembler_test.dart
+++ test/assembler_test.dart
@@ -2,8 +2,9 @@ import 'package:a_8085_helper/assembler/assembler.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  Assembler assembler = Assembler(code: "Hello");
-  test('Assembler Test', () {
-    expect(assembler.assemble(), "Hello");
+  Assembler assembler = Assembler();
+  test('Convert Assembly to opcode', () {
+    expect(assembler.assemble(code: "aci"), "CE");
+    expect(assembler.assemble(code: "adc d"), "8A");
   });
 }
